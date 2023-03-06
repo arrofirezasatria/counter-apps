9358bf7d40e35febfab6f1f1d58097bc370132b5
Arrofi Reza Satria <arrofirezasatria@gmail.com>
2023-03-06 17:46:48 +0700

    build: init project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+
+const DATA = [
+  { id: 1, data: 1 },
+  { id: 2, data: 2 },
+  { id: 3, data: 3 },
+];
+
+interface ICounter {
+  id: number;
+  text: string;
+  count: number;
+}
+
+interface ICounterList extends Array<ICounter> {}
 
 export default function App() {
+  const [enteredCounterText, setEnteredCounterText] = useState<string>("");
+  const [counterList, setCounterList] = useState<ICounterList>([]);
+
+  const textInputHandler = (currentText: string) => {
+    setEnteredCounterText(currentText);
+  };
+
+  const addCounterListHandler = () => {
+    setCounterList((prev) => [
+      ...prev,
+      {
+        id: 1,
+        text: enteredCounterText,
+        count: 12,
+      },
+    ]);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.containerAddCounter}>
+        <TextInput
+          style={styles.inputTextCounter}
+          onChangeText={textInputHandler}
+        />
+        <Button title="asdasd" onPress={addCounterListHandler} />
+      </View>
+      <View style={styles.containerCounterList}>
+        <FlatList
+          data={counterList}
+          renderItem={({ item }) => {
+            return (
+              <View>
+                <Text>ad</Text>
+              </View>
+            );
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +72,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  containerAddCounter: {
+    height: 210,
+    width: "100%",
+    backgroundColor: "gray",
+  },
+  containerCounterList: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "red",
+  },
+  inputTextCounter: {},
 });
